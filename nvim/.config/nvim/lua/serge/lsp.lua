17b93d375e4be9ddff2ca0107684c2fbc4ee6122
Serge Nasr <nasrserge@gmail.com>
2025-03-17 11:33:00 -0400

    neovim use default_capabilities

--- nvim/.config/nvim/lua/serge/lsp.lua
+++ nvim/.config/nvim/lua/serge/lsp.lua
@@ -27,7 +27,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup{
